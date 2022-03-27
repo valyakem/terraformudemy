1d91d2053efb997f5f6e734c3a6c127ef7fa1cf0
Valentine AKEM <valyakem@yahoo.co.uk>
2022-03-27 17:52:51 -0400

    Change the type

--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
 variable "vpc_cidr" {
   description                   = "Choose a vpc cidr block for your application or implementation"
-  type                          = "string"
+  type                          = string
   default                       = "10.0.0.0/16" 
 }
 
 variable "region" {
   description                   = "provide the default region to provision your resources"
-  type                          = "string" 
+  type                          = string
   default                       = "us-east-1" 
 }
 
