ecd5701de1842defc8d6f81c22ddd2e474d01938
Valentine AKEM <valyakem@yahoo.co.uk>
2022-03-27 18:02:34 -0400

    added some now

--- public_subnet.tf
+++ public_subnet.tf
@@ -1,9 +1,9 @@
 locals {
-  azs_name        = "${data.aws_availability_zones.azs.names[0]}"
+  azs_names        = "${data.aws_availability_zones.azs.names[0]}"
 } 
 
 resource "aws_subnet" "nb_publicsubnet" {
-  count             = "${length(local.azs_names.names[0])}"
+  count             = "${length(local.azs_names[0])}"
   vpc_id            = aws_vpc.nbvpc.id
   cidr_block        = "${cidrsubnet(var.cidr, 8, 1)}"
   availability_zone = "${local.az_names[count.index]}"
